2d98519c1e0de5677aa798bb17e20bae9dfb560a
Guillaume Boutin <guillaumeBoutin2101@gmail.com>
2015-10-19 23:16:39 +0000

    index.js writes response into data.json

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ var http = require('http');
 var request = require('request');
 var app = express();
 var bodyParser = require("body-parser");
+var jsonfile = require('jsonfile');
+
+var file = 'data.json';
 
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
   
   console.log(Object.keys(req.body));
+  var obj = req.body;
+  jsonfile.writeFile(file, obj, function (err) {
+    console.error(err)
+  });
   res.status(200).send('hello world');
   res.end('ending');
 });
@@ -20,3 +27,11 @@ var server = app.listen(process.env.PORT, process.env.IP, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+
+// var obj = {name: 'JS'}
+ 
+// jsonfile.writeFile(file, obj, function (err) {
+//   console.error(err)
+// });
