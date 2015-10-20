38a434221452846e665ab772e115e94514d4a351
Guillaume Boutin <guillaumeBoutin2101@gmail.com>
2015-10-20 00:12:49 +0000

    Accumulating pull requests into data.json file.

--- index.js
+++ index.js
@@ -10,28 +10,48 @@ var file = 'data.json';
 
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-  
+app.post('/', function(req, res) {
+
   console.log(Object.keys(req.body));
-  var obj = req.body;
-  jsonfile.writeFile(file, obj, function (err) {
-    console.error(err)
+
+  var data = {
+    action: req.body.action,
+    username: req.body.pull_request.user.login,
+    createdAt: req.body.pull_request.created_at,
+    closedAt: req.body.pull_request.closed_at,
+    title: req.body.pull_request.title
+  };
+
+  var dataArray = [];
+  jsonfile.readFile(file, function(err, obj) {
+    dataArray = obj;
+    dataArray.push(data);
+    console.log(dataArray);
+
+    jsonfile.writeFile(file, dataArray, function(err) {
+      console.error(err);
+      res.end();
+    });
+    
   });
-  res.status(200).send('hello world');
-  res.end('ending');
+
 });
 
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+
+
+// var dataArray = [];
+// jsonfile.readFile(file, function(err, obj) {
+//   dataArray = obj;
+//   console.log("inside");
+//   console.log(dataArray);
+// });
+
+// console.log("outside");
+// console.log(dataArray);
+
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
-
-// var obj = {name: 'JS'}
- 
-// jsonfile.writeFile(file, obj, function (err) {
-//   console.error(err)
-// });
