f4a484f500ee849941937e164ff3ef88df0393ce
Ziad Saab <ziad.saab@gmail.com>
2015-10-26 16:10:42 +0000

    global database connection

--- databases/forkeesToMongo.js
+++ databases/forkeesToMongo.js
@@ -1,53 +1,44 @@
-var MongoClient = require('mongodb').MongoClient;
+var db = require('./mongo');
 
 module.exports = {
-    
-    insert: function(reqBody) {
-        
-        MongoClient.connect('mongodb://' + process.env.IP + '/decodemtl', function(err, db) {
-    
-            db.collection("forkees")
-            .findOne({
-                "forkee.id": reqBody.forkee.id
-            }, function(err, res) {
-                
-                if (res) {
-                    console.log("This forkee hook is a duplicate. Not inserting it\n");
-                    db.close();
-                }
-                else {
-                    
-                    db.collection("members").findOne( {
-                        "id": reqBody.forkee.owner.id
-                    }, function(err, res1) {
-                        
-                        if(res1) {
-                            
-                            db.collection('forkees').insert(reqBody, function(err) {
-    
-                                if (err) {
-                                    console.error(err);
-                                }
-                            
-                                console.log("forkee hook inserted to MongoDb");
-                                db.close();
-                                
-                            });
 
-                        }
-                        else {
-                            console.log("This forkee is not from a member. Not inserting it.");
-                        }
-                        db.close();
+  insert: function(reqBody) {
+    db().collection("forkees")
+      .findOne({
+        "forkee.id": reqBody.forkee.id
+      }, function(err, res) {
 
-                    });
- 
+        if (res) {
+          console.log("This forkee hook is a duplicate. Not inserting it\n");
+        }
+        else {
+
+          db().collection("members").findOne({
+            "id": reqBody.forkee.owner.id
+          }, function(err, res1) {
+
+            if (res1) {
+
+              db().collection('forkees').insert(reqBody, function(err) {
+
+                if (err) {
+                  console.error(err);
                 }
-                
-            });
-            
-        });
-        
-    }
-   
+
+                console.log("forkee hook inserted to MongoDb");
+
+              });
+
+            }
+            else {
+              console.log("This forkee is not from a member. Not inserting it.");
+            }
+          });
+
+        }
+
+      });
+
+  }
+
 };
